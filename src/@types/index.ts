15c1f67af2c8f217aadbb735b1010bf6c1efcb7b
Thibaud Demay <8266439+ThibaudDemay@users.noreply.github.com>
2025-08-21 22:12:33 +0200

    Add index into @type folders

--- src/@types/index.ts
+++ src/@types/index.ts
@@ -1,3 +1,11 @@
-export * from './scraper/ollama-registry';
+
+// root @types
 export * from './settings';
 export * from './embedding';
+
+// react @types
+export * from './react/components';
+export * from './react/views';
+
+// scraper @types
+export * from './scraper/ollama-registry';
